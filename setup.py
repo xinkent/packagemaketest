a35f7e9f98a0efc6b80e8e63fc176cbf00854b37
xinkent <xinkent1994@gmail.com>
2019-02-18 10:51:41 +0900

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='packagemaketest',
-      version='0.1',
+      version='0.2',
       description='The funniest joke in Japan',
       url='http://github.com/xinkent/packagemaketest.git',
       author='K Xin',
